fe1b3604518fd96fd1e459b403851ee9c62864eb
José Marcelo <jmarceloapolinario09@gmail.com>
2026-06-01 07:30:26 -0300

    Initial commit

---
+++ src/main/java/com/jm/Avaliacoes_de_Livros/AvaliacoesDeLivrosApplication.java
@@ -0,0 +1,13 @@
+package com.jm.Avaliacoes_de_Livros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliacoesDeLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliacoesDeLivrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jm/Avaliacoes_de_Livros/AvaliacoesDeLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jm.Avaliacoes_de_Livros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliacoesDeLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
