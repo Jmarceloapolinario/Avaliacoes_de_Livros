535df7a38436d116597d1cb47d7250dbaf258c09
José Marcelo <jmarceloapolinario09@gmail.com>
2026-06-01 11:32:23 -0300

    feat: Criação do endpoint para salvar um novo comentario

---
+++ src/main/java/com/jm/Avaliacoes_de_Livros/Controller/ComentarioController.java
@@ -0,0 +1,40 @@
+package com.jm.Avaliacoes_de_Livros.Controller;
+
+import com.jm.Avaliacoes_de_Livros.Controller.Request.ComentariosRequest;
+import com.jm.Avaliacoes_de_Livros.Controller.Response.ComentariosResponse;
+import com.jm.Avaliacoes_de_Livros.Controller.Response.LivrosResponse;
+import com.jm.Avaliacoes_de_Livros.Mapper.ComentariosMapper;
+import com.jm.Avaliacoes_de_Livros.Mapper.LivrosMapper;
+import com.jm.Avaliacoes_de_Livros.Model.Comentarios;
+import com.jm.Avaliacoes_de_Livros.Model.Livros;
+import com.jm.Avaliacoes_de_Livros.Service.ComentariosService;
+import com.jm.Avaliacoes_de_Livros.Service.LivrosService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.PublicKey;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("livros/comentarios")
+@RequiredArgsConstructor
+public class ComentarioController {
+
+    private final ComentariosService service;
+    private final LivrosService livrosService;
+
+    @PostMapping
+    public ResponseEntity<ComentariosResponse> save(@RequestBody ComentariosRequest request){
+        Comentarios comentario = ComentariosMapper.toComentario(request);
+
+        Livros livro = livrosService.findById(request.livroId());
+        comentario.setLivro(livro);
+
+        Comentarios savedComentario = service.save(comentario);
+        return ResponseEntity.ok(ComentariosMapper.toComentarioResponse(savedComentario));
+    }
+
+}
